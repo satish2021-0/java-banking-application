bbbb5043a4dd5579ce88f3065d22e352a02869c4
satis <satis@Satish>
2026-01-06 17:48:28 +0530

    Initial commit - Core Java Banking Application

---
+++ bank_app/src/domain/Type.java
@@ -0,0 +1,10 @@
+
+package domain;
+
+///Definition of enum:- It is a list of fix constant and it is used to restrict values at compile time so that invalid data never enters the system.
+public enum Type {
+    DEPOSIT,
+    WITHDRAW,
+    TRANSFER_IN,
+    TRANSFER_OUT
+}
